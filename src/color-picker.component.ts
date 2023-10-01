31bf3f407ecf20450de7c3ae7e36546c66469689
maza <maza@ailleron.com>
2023-10-01 20:20:32 +0200

    control value accessor fix

--- src/color-picker.component.ts
+++ src/color-picker.component.ts
@@ -1,31 +1,39 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, forwardRef } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, forwardRef } from "@angular/core";
 import {
   NG_VALUE_ACCESSOR,
   ControlValueAccessor,
   ReactiveFormsModule,
   FormsModule,
-} from '@angular/forms';
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-color-picker',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-color-picker",
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
-      useValue: forwardRef(() => AppColorPickerComponent),
+      useExisting: forwardRef(() => AppColorPickerComponent),
       multi: true,
     },
   ],
   template: `
-    <input type="color" [value]="value" (change)="colorChange($event)" />
+    <input
+      type="color"
+      [ngModel]="value"
+      [value]="value"
+      (change)="colorChange($any($event.target).value)"
+    />
+    <p>Value: {{ value }}</p>
   `,
 })
 export class AppColorPickerComponent implements ControlValueAccessor {
   value: string | null = null;
-  onChange: (value: string | null) => void = () => {};
+  onChange: (value: string | null) => void = (value) => {
+    console.log("onChange", value);
+  };
   onTouched: () => void = () => {};
 
   registerOnChange(fn: any): void {
@@ -37,13 +45,13 @@ export class AppColorPickerComponent implements ControlValueAccessor {
   }
 
   writeValue(val: string | null) {
-    console.log('write value', val);
+    console.log("write value", val);
     this.value = val || null;
   }
 
-  colorChange(event: any) {
-    this.value = event.target.value;
-    console.log(this.value);
+  colorChange(value: any) {
+    this.value = value;
+    console.log("colorChange", value);
     this.onTouched();
     this.onChange(this.value);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import "zone.js/dist/zone";
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { bootstrapApplication } from "@angular/platform-browser";
 import {
   FormControl,
   FormsModule,
   ReactiveFormsModule,
   Validators,
-} from '@angular/forms';
-import { AppColorPickerComponent } from './color-picker.component';
+} from "@angular/forms";
+import { AppColorPickerComponent } from "./color-picker.component";
 
 @Component({
-  selector: 'my-app',
+  selector: "my-app",
   standalone: true,
   imports: [
     CommonModule,
@@ -20,19 +20,23 @@ import { AppColorPickerComponent } from './color-picker.component';
     AppColorPickerComponent,
   ],
   template: `
-    <h1> Pick a color</h1>
-    <app-color-picker [formControl]="colorFc"/>
-    <p>Current color: <b>{{colorFc.value}}</b></p>
-    <button (click)="setCustom()"> Set to #123456</button>
+    <h1>Pick a color</h1>
+    <app-color-picker [formControl]="colorFc" />
+    <p>
+      Current color: <b>{{ colorFc.value }}</b>
+    </p>
+    <button (click)="setCustom()">Set to #123456</button>
   `,
 })
 export class App {
-  colorFc = new FormControl('#fff', Validators.required);
+  colorFc = new FormControl("#ffffff", Validators.required);
 
   setCustom() {
-    this.colorFc.setValue('#123456');
+    this.colorFc.setValue("#123456");
     this.colorFc.updateValueAndValidity();
   }
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App, {
+  providers: [],
+});
